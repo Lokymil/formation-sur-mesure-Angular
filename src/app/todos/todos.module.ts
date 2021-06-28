aca7fa1e7fa699111436737c0e701bdc574c0d47
Lokymil <arrestierchristophe@gmail.com>
2021-06-28 16:12:48 +0200

    Add link between list and update form

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -4,9 +4,10 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
 import { TodoFormComponent } from './todo-form/todo-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
   declarations: [TodoListComponent, TodoItemComponent, TodoFormComponent],
 })
 export class TodosModule {}
